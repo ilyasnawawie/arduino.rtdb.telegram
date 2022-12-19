7da8b294b879d627c9b7073907c45786b00b4487
Ilyas <92800413+ilyasnawawie@users.noreply.github.com>
2022-12-19 18:50:48 +0800

    final complete changes (hopefully)

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,63 +5,40 @@ import * as admin from "firebase-admin";
 
 admin.initializeApp();
 const bot = express();
-
 bot.use(cors({origin: true}));
 
-bot.post("/", async function(req, res) {
-  if (
-    req.body &&
-    req.body.message &&
-    req.body.message.chat &&
-    req.body.message.chat.id &&
-    req.body.message.from &&
-    req.body.message.from.first_name
-  ) {
+bot.post("/", async (req, res) => {
+  if (req.body && req.body.message && req.body.message.chat && req.body.message.chat.id && req.body.message.from && req.body.message.from.first_name) {
     const chatId = req.body.message.chat.id;
     const userName = req.body.message.from.first_name;
     const message = req.body.message.text;
 
-    // Check if the received message is the "/start" command
     if (message === "/start") {
-      // Define your RTDB Reference to point to the "Sensor MQ7" parent node
       const rtdbRef = admin.database().ref("Sensor MQ7");
-
-      // Read the latest unknown child node of "Sensor MQ7"
-      const latestUnknownChildSnapshot = await rtdbRef
-          .limitToLast(1)
-          .once("child_added");
-
-      // Read the known child nodes of the latest unknown child node
+      const latestUnknownChildSnapshot = await rtdbRef.limitToLast(1).once("child_added");
       const coConcentrationSnapshot = latestUnknownChildSnapshot.child("MQ7");
       const latitudeSnapshot = latestUnknownChildSnapshot.child("latitude");
       const longitudeSnapshot = latestUnknownChildSnapshot.child("longitude");
       const timeSnapshot = latestUnknownChildSnapshot.child("time");
-
-      // Get the values of the known child nodes
       const coConcentration = coConcentrationSnapshot.val();
       const latitude = latitudeSnapshot.val();
       const longitude = longitudeSnapshot.val();
       const time = timeSnapshot.val();
 
-      // Check if the known child node value is above 100
       if (coConcentration > 100) {
         const mapsLink = `https://www.google.com/maps/search/?api=1&query=${latitude},${longitude}`;
         const encodedMapsLink = encodeURI(mapsLink);
-        // Update the text and callback data of the button to include the NP value
-        const buttonText = `Call ${userName}`;
-        const callbackData = `call_user_${userName}`;
 
-        // Return the response with a button interface
         return res.status(200).send({
           method: "sendMessage",
           chat_id: chatId,
-          text: `${userName} berada dalam bahaya. Tahap konsentrasi karbon monoksida pada masa ini adalah ${coConcentration}. Lokasi terakhir ada di (${encodedMapsLink}) pada pukul ${time}.`,
+          text: `${userName} mungkin berada dalam bahaya. Konsentrasi Carbon Monoxida tinggi dikesan pada skala ${coConcentration}ppm. Lokasi terakhir adalah (${encodedMapsLink}) pada waktu ${time}.`,
           reply_markup: {
             inline_keyboard: [
               [
                 {
-                  text: buttonText,
-                  callback_data: callbackData,
+                  text: "Maklumat Lanjut",
+                  url: "https://mq7fyp.000webhostapp.com/",
                 },
               ],
             ],
@@ -70,12 +47,11 @@ bot.post("/", async function(req, res) {
       }
     }
   } else {
-    // Return an error message if the request body is missing the required information
     return res.status(400).send({
       status: "An error occurred. The request body is missing the required information.",
     });
   }
-  // Return an empty response
   return res.status(200).send({});
 });
+
 export const router = functions.https.onRequest(bot);
